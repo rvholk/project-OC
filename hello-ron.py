b6233a8ae8af387fbc1aceea2c0db56057f0a8d4
rvholk <rvholk@multiweb.nl>
2020-11-04 11:15:20 +0100

    modified:   hello-ron.py added date output

--- hello-ron.py
+++ hello-ron.py
@@ -1,6 +1,9 @@
 import time
+import datetime
+today = []
+now = datetime.date.today()
+today.append(now)
 
 while True:
-
-    print ("Hello Ron, How is your day?")
+    print ("Hello Ron, How is your day? Today is: " + str(today[0]))
     time.sleep(10)
